7c8131fb9b711c726f512029f94a25d1c4a13f51
zhangmike <zhangmike@sina.com>
2016-01-18 06:36:05 +0800

    add applicationContext.xml only

--- springmaster/src/main/java/com/agilejerry/springmaster/App.java
+++ springmaster/src/main/java/com/agilejerry/springmaster/App.java
@@ -1,15 +1,25 @@
 package com.agilejerry.springmaster;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext(   
+                "applicationContext.xml"); 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+        SpringApplication.run(App.class, args);
     }
 }
--- springmaster/src/test/java/com/agilejerry/springmaster/AppTest.java
+++ springmaster/src/test/java/com/agilejerry/springmaster/AppTest.java
@@ -7,6 +7,14 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
 public class AppTest 
 {
 
@@ -20,4 +28,7 @@ public class AppTest
         assertTrue( true );
     }
     
+	@Test
+	public void contextLoads() {
+	}
 }
